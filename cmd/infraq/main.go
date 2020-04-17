3d4849656c90d4718edfcaf4b601dcf68ab0d30f
Nathan Fisher <nfisher@junctionbox.ca>
2020-04-17 04:19:50 -0300

    Add Si units to chart output

--- cmd/infraq/main.go
+++ cmd/infraq/main.go
@@ -80,6 +80,7 @@ func main() {
 	log.Printf("API Key Set: %v\n", apiToken != "")
 	log.Printf("API URL:     %v\n", apiURL)
 	log.Printf("Metric:      %v\n", metricName)
+	log.Printf("Plugin:      %v\n", pluginType)
 	log.Printf("Query:       %v\n", queryString)
 	log.Printf("Rollup:      %v\n", time.Duration(rollup)*time.Second)
 	log.Printf("To:          %v\n", toString)
@@ -103,11 +104,11 @@ func main() {
 
 func writeCharts(metrics []openapi.MetricItem, metricName string) {
 	for _, item := range metrics {
-		prefix := item.Host
+		shortName := shortenMetric(metricName)
+		prefix := strings.Replace(item.Host, ":", "-", -1) + "-" + shortName
 		if prefix == "" {
 			prefix = strings.Replace(item.Label, "/", "-", -1)
 		}
-		//log.Printf("%s %v\n", prefix, item.Metrics[metricName])
 
 		lineChart := newChart(&item, metricName)
 		if lineChart == nil {
@@ -162,13 +163,15 @@ func renderChart(name string, lineChart *chart.Chart) error {
 func newChart(item *openapi.MetricItem, metricName string) *chart.Chart {
 	metricsLen := len(item.Metrics[metricName])
 	if metricsLen < 2 {
-		log.Printf("no metrics available: %s\n", item.Host)
+		log.Printf("no metrics available: %s:%s\n", item.Host, item.Label)
 		return nil
 	}
-	fmt.Println("len =", metricsLen)
+
 	xValues := make([]float64, metricsLen)
 	yValues := make([]float64, metricsLen)
 
+	var min = math.MaxFloat64
+	var max = math.SmallestNonzeroFloat64
 	var metric = item.Metrics[metricName]
 	var previous float64
 	for i, v := range metric {
@@ -176,6 +179,13 @@ func newChart(item *openapi.MetricItem, metricName string) *chart.Chart {
 		var value = v[SeriesValue]
 		if math.IsInf(value, 0) || math.IsNaN(value) {
 			value = 0.0
+		}
+
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
 		}
 
 		xValues[i] = timestamp
@@ -186,18 +196,19 @@ func newChart(item *openapi.MetricItem, metricName string) *chart.Chart {
 		}
 		previous = timestamp
 	}
+	fmt.Println("len =", metricsLen, item.Label, " delta=", max-min)
 
-	title := item.Host
-	if title == "" {
-		title = item.Label
-	}
+	title := item.Label
 
-	return &chart.Chart{
+	graph := &chart.Chart{
 		Title:      title,
 		TitleStyle: chart.StyleShow(),
 		Background: chart.Style{
 			Padding: chart.Box{
-				Top: 75,
+				Top:    75,
+				Bottom: 35,
+				Right:  25,
+				Left:   25,
 			},
 		},
 		XAxis: chart.XAxis{
@@ -207,18 +218,50 @@ func newChart(item *openapi.MetricItem, metricName string) *chart.Chart {
 			ValueFormatter: func(v interface{}) string { return time.Unix(int64(v.(float64))/1000, 0).UTC().Format("15:04:05") },
 		},
 		YAxis: chart.YAxis{
-			Name:           metricName,
 			NameStyle:      chart.StyleShow(),
 			Style:          chart.StyleShow(),
-			ValueFormatter: func(v interface{}) string { return chart.FloatValueFormatterWithFormat(v, "%.4f") },
+			ValueFormatter: yFormatter(min),
 		},
-		Width:  800,
-		Height: 494,
+		Width:  900,
+		Height: 550,
 		Series: []chart.Series{
 			chart.ContinuousSeries{
 				XValues: xValues,
 				YValues: yValues,
+				Name:    metricName,
 			},
 		},
 	}
+
+	graph.Elements = []chart.Renderable{
+		chart.Legend(graph),
+	}
+
+	return graph
+}
+
+func yFormatter(min float64) chart.ValueFormatter {
+	units := []string{"", "Ki", "Mi", "Gi", "Ti"}
+	return func(v interface{}) string {
+		if min < 1.0 {
+			return chart.FloatValueFormatterWithFormat(v, "%.4f")
+		} else if min < 1000.0 {
+			return chart.FloatValueFormatterWithFormat(v, "%.2f")
+		}
+		val, ok := v.(float64)
+		if !ok {
+			log.Fatalln("not ok!")
+		}
+		idx := math.Log10(val) / math.Log10(1000.0)
+		unit := units[int(idx)]
+		return chart.FloatValueFormatterWithFormat(val/math.Pow(1000, math.Floor(idx)), "%.2f") + unit
+	}
+}
+
+func shortenMetric(metricName string) string {
+	if len(metricName) < 60 {
+		return metricName
+	}
+	metricSplit := strings.Split(metricName, ".")
+	return metricSplit[len(metricSplit)-1]
 }
